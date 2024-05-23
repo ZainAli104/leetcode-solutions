e817c8ae35272dadee31ccafa2d5705152bd75c1
ZainAli <chzainali6363@gmail.com>
2024-05-23 21:37:33 +0500

    complete => 443 problem

--- solutions/443. String Compression.js
+++ solutions/443. String Compression.js
@@ -2,29 +2,35 @@ var compress = function(chars) {
     let s = "";
     let count = 0;
     let result = [];
+    const charsLength = chars.length;
     
-    for (let i=0; i<chars.length; i++) {
+    for (let i=0; i<charsLength; i++) {
         if (s===chars[i]) {
             count++
-            chars.splice(i, 1)
-            if (i+1==chars.length) {
-                result.push(count);
+            if (i+1==charsLength) {
+                const res = count.toString().split('')
+                result.push(...res)
+                chars.push(...res)
             }
         } else {
-            if (count!==0) {
-                result.push(count);
+            if (count!==0 && count!==1) {
+                const res = count.toString().split('')
+                result.push(...res)
+                chars.push(...res)
             }
             count = 1;
             s = chars[i];
-            result.push(s);
+            result.push(s)
+            chars.push(s)
         }
     }
-
+    
+    chars.splice(0, charsLength)
     console.log(result)
     return result.length;
 };
 
-let arr = ["a","a","b","b","c","c","c"];
+let arr = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
 
 console.log(compress(arr));
 console.log(arr)
